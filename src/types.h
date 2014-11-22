22d3cbe584a5f4f04a3af0568546e926ca92758a
Dave Murphy <davem@devkitpro.org>
2014-11-22 14:41:13 +0000

    bswap functions, not available with gcc 4.0 OSX

--- src/types.h
+++ src/types.h
@@ -13,16 +13,28 @@ typedef uint8_t u8;
 
 #define BIT(n) (1U << (n))
 
+static inline uint16_t __local_bswap16(uint16_t x) {
+	return ((x << 8) & 0xff00) | ((x >> 8) & 0x00ff);
+}
+
+
+static inline uint32_t __local_bswap32(uint32_t x) {
+	return	((x << 24) & 0xff000000 ) |
+			((x <<  8) & 0x00ff0000 ) |
+			((x >>  8) & 0x0000ff00 ) |
+			((x >> 24) & 0x000000ff );
+}
+
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#define be_word(a)  __builtin_bswap32(a)
-#define be_hword(a) __builtin_bswap16(a)
+#define be_word(a)  __local_bswap32(a)
+#define be_hword(a) __local_bswap16(a)
 #define le_word(a)  (a)
 #define le_hword(a) (a)
 #elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
 #define be_word(a)  (a)
 #define be_hword(a) (a)
-#define le_word(a)  __builtin_bswap32(a)
-#define le_hword(a) __builtin_bswap16(a)
+#define le_word(a)  __local_bswap32(a)
+#define le_hword(a) __local_bswap16(a)
 #else
 #error "What's the endianness of the platform you're targeting?"
 #endif
