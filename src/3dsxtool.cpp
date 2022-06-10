c7fdeff613152b0e1fe0b7dc3888bf791e30e131
TurtleP <jpostelnek@outlook.com>
2022-06-10 12:00:54 -0400

    update gitignore (.vscode folder) and usage prompt

--- src/3dsxtool.cpp
+++ src/3dsxtool.cpp
@@ -561,7 +561,7 @@ int usage(const char* progName)
 		"    %s input.elf output.3dsx [options]\n\n"
 		"Options:\n"
 		"    --smdh=input.smdh : Embeds SMDH metadata into the output file.\n"
-		"    --romfs=dir       : Embeds RomFS into the output file.\n"
+		"    --romfs=dir/file  : Embeds RomFS into the output file.\n"
 		, progName);
 	return 1;
 }
