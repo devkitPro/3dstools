e7da1b966c17f16d9678b684c186790f20cffb76
Dave Murphy <davem@devkitpro.org>
2022-08-16 00:00:49 +0100

    3dsxtool: include headers for stat

--- src/3dsxtool.cpp
+++ src/3dsxtool.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #include <vector>
 #include <map>
 #include <list>
